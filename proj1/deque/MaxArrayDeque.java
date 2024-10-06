26038468a55912ab50877d2c779790c272f98cb6
xia <3415671317@qq.com>
2024-10-07 08:42:02 +1100

    maxarraydeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,5 +1,5 @@
 package deque;
-/**
+
 public class MaxArrayDeque<T> extends ArrayDeque<T>{
     public Comparator<T> Comparator;
     public MaxArrayDeque(Comparator<T> c){
@@ -8,10 +8,29 @@ public class MaxArrayDeque<T> extends ArrayDeque<T>{
 
     }
     public T max(){
+        if (this.isEmpty()){
+            return null;
+        }
+        T  max = this.get(0);
+        for (int i  = 1; i < this.size(); i++){
+            if (this.Comparator.compare(max, this.get(i)) < 0){
+                max = this.get(i);
+            }
 
+        }
+        return max;
     }
     public T max(Comparator<T> c){
+        if (this.isEmpty()){
+            return null;
+        }
+        T  max = this.get(0);
+        for (int i  = 1; i < this.size(); i++){
+            if (c.compare(max, this.get(i)) < 0){
+                max = this.get(i);
+            }
 
+        }
+        return max;
     }
 }
-*/
